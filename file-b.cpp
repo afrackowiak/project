1b368e4a708033cccd13c9d1d1729d84d63a86aa
Anna <anna.frckowiak@gmail.com>
2023-04-17 13:15:39 +0000

    Add 'file-b.cpp' and 'file-c.cpp'

--- file-b.cpp
+++ file-b.cpp
@@ -1,1 +1,32 @@
-Hello, world!
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello world" <<endl;
+    /* druga
+    czesc
+    programu */
+    cout <<"Podaj imie: " <<endl;
+    string imie;
+    cin >>imie; //wprowadzam imie z klawiatury
+    cout << "Hello " << imie << endl;
+
+    /*trzecia czesc 
+    programu */
+    int a;
+    float b;
+    char c;
+    a = 10.567;
+    c = '$';
+    cout << "Wprowadz liczbe rzeczywista: " << endl;
+    cin >> b;
+    cout << "a = " << a << " rozmiar zmiennej: " << sizeof(a) << endl;
+    cout << "b = " << fixed << setprecision (2) <<b << endl;
+    printf("b = %.2f\n", b);
+    cout << "c = " << c << " rozmiar zmiennej: " << sizeof(c) << endl;
+return 0;
+}
--- file-c.cpp
+++ file-c.cpp
@@ -1,1 +1,31 @@
-petla
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    //start, wypisuje kilka pierwszych wywolan plus ostatnie
+    cout << "2 to the power of 0 is 1"<<endl;
+    cout << "2 to the power of 1 is 2"<<endl;
+    cout << "2 to the power of 2 is 4"<<endl;
+    cout << "2 to the power of 3 is 8"<<endl;
+    // ...
+    cout << "2 to the power of 16 is 65536"<<endl;
+
+cout << endl;
+// petla for
+for (int i=0; i<=16; i++) {
+    cout << "2 to the power of " << i << "is" <<pow (2, i) <<endl;
+    }
+
+    cout << endl;
+    // petla while
+    int j=0;
+    while (j<=16) {
+    cout << "2 to the power of " << j << "is" <<pow (2, j) <<endl;
+    j++;
+    }
+
+    return 0;
+}
